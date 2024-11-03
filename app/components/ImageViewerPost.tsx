9a3ef145c5f0bad4bb4681bebcb4f3d6f1b44681
Anshuman <48919248+anshuman0799@users.noreply.github.com>
2024-11-03 12:51:26 -0500

    Improved caption placement in post collage

--- app/components/ImageViewerPost.tsx
+++ app/components/ImageViewerPost.tsx
@@ -112,7 +112,7 @@ const ImageViewer: React.FC<ImageViewerProps> = ({
           </button>
         )}
         {/* Display creator and prompt */}
-        <div className="absolute bottom-10 lg:bottom-0 left-25 bg-black bg-opacity-60 p-2 rounded-md flex flex-col w-[90%] lg:w-[70%] overflow-hidden justify-center">
+        <div className="absolute bottom-0 lg:bottom-0 left-25 bg-black bg-opacity-60 p-2 rounded-md flex flex-col w-[90%] lg:w-[70%] overflow-hidden justify-center">
           <div className="flex gap-1 items-center text-white">
             <FaUser className="mr-1" />
             <h2 className="text-xs lg:text-sm">{creator}</h2>
